bb7b9066a9d5dc2c5b9feeb344c5ddd7f35a48c9
Chris - Work <cisherwood@CISHERWOOD-PC>
2016-12-15 11:04:47 -0500

    testaddmodel

---
+++ PCA/PCA/Models/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PCA.Models
+{
+    public class Test
+    {
+        public int TestId { get; set; }
+        public string Place { get; set; }
+    }
+}
