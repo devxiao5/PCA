d5121de21b0fb5f06b32b3af3046f37564d2a648
Chris - Work <cisherwood@CISHERWOOD-PC>
2016-12-26 17:45:19 -0500

    12-26end

--- PCA/PCA/Controllers/InvoiceController.cs
+++ PCA/PCA/Controllers/InvoiceController.cs
@@ -21,10 +21,77 @@ namespace PCA.Controllers
             ViewBag.CurrentProjectString = currentList.ElementAt(0);
             ViewBag.CurrentProjectNumber = int.Parse(currentList.ElementAt(1));
             //---------------------------
+            // Declarations
+            int currentProjectNumber = ViewBag.CurrentProjectNumber;
+
+            List<Invoice> invoices = new List<Invoice>(from invoice in db.Invoices
+                                                       where invoice.ProjectId == currentProjectNumber
+                                                       select invoice);
+
+
+            return View(invoices);
+        }
+
+        // GET: Invoices/Create
+        public ActionResult Create()
+        {
+            //Get current project
+            var systemController = DependencyResolver.Current.GetService<SystemController>();
+            systemController.Get();
+            var currentList = systemController.Get();
+            ViewBag.CurrentProjectString = currentList.ElementAt(0);
+            ViewBag.CurrentProjectNumber = int.Parse(currentList.ElementAt(1));
+            //---------------------------
+
+            ViewBag.ContractorId = new SelectList(db.Contractors, "ContractorId", "Name");
+            ViewBag.ProjectId = new SelectList(db.Projects, "ProjectId", "Name");
             return View();
         }
 
-        public ActionResult Create()
+        // POST: Invoices/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "InvoiceId,ProjectId,ContractorId,AIANumber,InvoiceNumber,AccountNumber,OrderNumber,TotalAmount,TermInDays,DateReceived,DateOfInvoice,Status")] Invoice invoice, HttpPostedFileBase upload)
+        {
+            //Get current project
+            var systemController = DependencyResolver.Current.GetService<SystemController>();
+            systemController.Get();
+            var currentList = systemController.Get();
+            ViewBag.CurrentProjectString = currentList.ElementAt(0);
+            ViewBag.CurrentProjectNumber = int.Parse(currentList.ElementAt(1));
+            //---------------------------
+
+            if (ModelState.IsValid)
+            {
+                if (upload != null && upload.ContentLength > 0)
+                {
+                    var invoiceUpload = new File
+                    {
+                        FileName = System.IO.Path.GetFileName(upload.FileName),
+                        FileType = FileType.InvoiceImage,
+                        ContentType = upload.ContentType
+                    };
+
+                    using (var reader = new System.IO.BinaryReader(upload.InputStream))
+                    {
+                        invoiceUpload.Content = reader.ReadBytes(upload.ContentLength);
+                    }
+                    invoice.Files = new List<File> { invoiceUpload };
+
+                }
+                db.Invoices.Add(invoice);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ContractorId = new SelectList(db.Contractors, "ContractorId", "Name", invoice.ContractorId);
+            ViewBag.ProjectId = new SelectList(db.Projects, "ProjectId", "Name", invoice.ProjectId);
+            return View(invoice);
+        }
+
+        /*public ActionResult Create()
         {
             //Get current project
             var systemController = DependencyResolver.Current.GetService<SystemController>();
@@ -55,7 +122,11 @@ namespace PCA.Controllers
 
             ViewBag.ProjectId = new SelectList(db.Projects, "ProjectId", "Name");
             return View();
-        }
+        }*/
+
+
+
+
     }
 
 
---
+++ PCA/PCA/ViewModels/BudgetDetailReportViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PCA.Models;
+
+namespace PCA.ViewModels
+{
+    public class BudgetDetailReportViewModel
+    {
+        /*public List<Phase> Phases { get; set; }
+        public List<Budget> Budgets { get; set; }
+        public List<double> TotalCost { get; set; }*/
+
+
+        public int PhaseId { get; set; }
+        public string PhaseName { get; set; }
+        public string PhaseNumber { get; set; }
+        public string BudgetDescription { get; set; }
+        public double TotalCost { get; set; }
+
+        public BudgetDetailReportViewModel(int pid, string pname, string pnum, string des, double tc)
+        {
+            this.PhaseId = pid;
+            this.PhaseName = pname;
+            this.PhaseNumber = pnum;
+            this.BudgetDescription = des;
+            this.TotalCost = tc;
+        }
+
+    }
+}
